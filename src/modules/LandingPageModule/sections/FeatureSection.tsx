eaf894c522adb3d3b2630d1b985172c8126bc17a
matthewhjt <matthewhjt72@gmail.com>
2025-02-08 04:18:42 +0700

    init feature section

--- src/modules/LandingPageModule/sections/FeatureSection.tsx
+++ src/modules/LandingPageModule/sections/FeatureSection.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export const FeatureSection: React.FC = () => {
-  return <section>FeatureSection</section>
+  return (
+    <section className='bg-gradient-to-br from-[#0073E6] to-[#0059B3] text-white flex flex-col items-center justify-center'>
+      <h1>FEATURES</h1>
+      <div></div>
+    </section>
+  )
 }
