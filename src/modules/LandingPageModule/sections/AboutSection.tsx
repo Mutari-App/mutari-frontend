440d7565a208e17a96e77e25a01a213fc29ff93e
daffarafip24 <daffarafip24@gmail.com>
2025-02-10 13:22:48 +0700

    feat(about): styling

--- src/modules/LandingPageModule/sections/AboutSection.tsx
+++ src/modules/LandingPageModule/sections/AboutSection.tsx
@@ -4,28 +4,30 @@ import React from 'react'
 
 export const AboutSection: React.FC = () => {
   return (
-    <section className="min-h-screen w-full flex justify-center items-center">
+    <section className="w-full sm:mb-8 md:mb-14 lg:mb-24 xl:mb-32 flex justify-center items-center py-10 sm:pb-20 overflow-hidden">
       <div
-        className="box-content w-4/5 h-auto min-h-72 p-4 mx-auto bg-white shadow-md space-y-10 border rounded-md border-gray-300 flex justify-between gap-3"
+        className="box-content  max-w-screen-lg relative w-4/5 h-auto min-h-72 px-4 py-2 flex-col md:flex-row xl:py-12 mx-auto bg-white shadow-md  border rounded-md border-gray-300 flex md:justify-evenly items-center gap-3"
         style={{ borderColor: '#0073E6' }}
       >
-        <div className="relative w-full md:w-1/3">
-          <div className="w-full flex justify-center md:absolute md:-top-16 md:-left-8">
+        <div className="relative w-full h-min md:w-1/3">
+          <div className="w-full flex justify-center md:absolute -translate-y-1/3 md:-translate-y-1/2 max-w-sm mx-auto md:max-w-none md:-left-10 scale-110">
             <Image
               src={getImage('img-about.png')}
               alt="About"
               width={720}
-              height={480}
-              className="w-[80%] md:w-auto h-auto shadow-md"
+              height={600}
+              className="w-[90%] md:w-auto aspect-video md:aspect-auto object-cover shadow-md"
             />
           </div>
         </div>
-        <div className="w-full md:w-3/5 text-center mr-10 md:text-center space-y-4">
-          <h2 className="text-3xl font-bold text-blue-600">ABOUT US</h2>
-          <p className="text-blue-600 text-sm">
+        <div className="w-full md:w-3/5 text-center  mb-6 md:mb-0 -mt-10 md:mt-0 md:mr-6  flex flex-col items-center justify-center relative">
+          <h2 className="text-lg sm:text-3xl font-bold text-blue-600">
+            ABOUT US
+          </h2>
+          <p className="text-blue-600 text-xs sm:text-base">
             <strong>
               Ingin liburan ke Indonesia tapi bingung menyusun rencana
-              perjalanan?
+              perjalanan?{' '}
             </strong>
             Mutari hadir sebagai solusi terbaik untuk membuat pengalaman wisata
             Anda lebih mudah dan menyenangkan! Kami menawarkan berbagai fitur
@@ -37,13 +39,12 @@ export const AboutSection: React.FC = () => {
             lebih praktis dan tanpa ribet!
           </p>
         </div>
-
         <Image
           src={getImage('logoblue-background.png')}
           alt="Icon"
-          width={100}
-          height={100}
-          className="absolute -bottom-0.5 right-10 w-[100px] md:w-[100px]"
+          width={160}
+          height={160}
+          className="absolute hidden sm:block translate-x-1/2 translate-y-1/3 bottom-0 right-0 w-[100px] lg:w-[120px]"
         />
       </div>
     </section>
