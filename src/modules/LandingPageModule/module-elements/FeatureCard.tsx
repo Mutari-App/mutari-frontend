6e272c45abfce5aecb83b2e70f1d511911e655d9
daffarafip24 <daffarafip24@gmail.com>
2025-02-12 16:43:10 +0700

    fix: adjust feature content text position

--- src/modules/LandingPageModule/module-elements/FeatureCard.tsx
+++ src/modules/LandingPageModule/module-elements/FeatureCard.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const FeatureCard: React.FC<FeatureCardProps> = (feature) => {
   return (
-    <div className="shadow-[0px_3.25px_12px_rgba(1,95,189,1)] relative w-full min-h-60 text-white bg-gradient-to-br from-white/40 from-[10%] to-white/5 rounded-3xl overflow-hidden flex flex-col items-center md:items-start justify-start md:justify-center pt-[50%] md:pt-[35%] pb-[10%] px-[10%] lg:pl-[40%] xl:pl-[35%] lg:pr-[10%] lg:py-5 space-y-2 ">
+    <div className="shadow-[0px_3.25px_12px_rgba(1,95,189,1)] relative w-full min-h-60 text-white bg-gradient-to-br from-white/40 from-[10%] to-white/5 rounded-3xl overflow-hidden flex flex-col items-center md:items-start justify-start md:justify-center pt-16 md:pt-[35%] pb-[10%] px-[10%] lg:pl-[40%] xl:pl-[35%] lg:pr-[10%] lg:py-5 space-y-2 ">
       <div className="absolute rounded-full -top-[90px] md:-top-[90px] -left-[100px] md:-left-[100px] w-[230px] md:w-[250px] lg:w-[350px] h-[230px] md:h-[250px] lg:h-[350px] outline outline-2 outline-white/10"></div>
       <div className="absolute rounded-full -top-[75px] md:-top-[75px] -left-[75px] md:-left-[75px] w-[180px] md:w-[200px] lg:w-[275px] h-[180px] md:h-[200px] lg:h-[275px] outline outline-2 outline-white/10"></div>
       <div className="absolute rounded-full -top-[50px] md:-top-[50px] -left-[50px] md:-left-[50px] w-[130px] md:w-[150px] lg:w-[200px] h-[130px] md:h-[150px] lg:h-[200px] bg-gradient-to-br from-white/0 from-[20%] to-white/40"></div>
--- src/modules/LandingPageModule/sections/FeatureSection.tsx
+++ src/modules/LandingPageModule/sections/FeatureSection.tsx
@@ -7,7 +7,7 @@ export const FeatureSection: React.FC = () => {
     <section className="bg-gradient-to-b from-[#2FA9F4] to-[#0059B3] text-white flex flex-col items-center justify-center px-[10%] py-[5%] gap-8">
       <h1 className="text-3xl font-semibold">FITUR KAMI</h1>
 
-      <div className="grid grid-cols-2 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
         {FEATURES.map((feature, index) => (
           <FeatureCard
             key={index}
