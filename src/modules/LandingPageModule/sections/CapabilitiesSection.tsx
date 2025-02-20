854e05f24cee3d573db2dbc53023aeb9dae91893
daffarafip24 <daffarafip24@gmail.com>
2025-02-20 19:08:46 +0700

    feat(landing): responsiveness

--- src/modules/LandingPageModule/sections/CapabilitiesSection.tsx
+++ src/modules/LandingPageModule/sections/CapabilitiesSection.tsx
@@ -5,7 +5,7 @@ import { CapabilityItem } from '../module-elements/CapabilityItem'
 export const CapabilitiesSection: React.FC = () => {
   return (
     <section className="relative my-24 container px-4 max-w-screen-xl mx-auto text-white">
-      <div className="grid grid-cols-3 gap-16 ">
+      <div className="grid grid-cols-1 max-w-md mx-auto md:max-w-none md:grid-cols-3 gap-8 sm:gap-12 md:gap-16 ">
         {CAPABILITIES.map((capability) => (
           <CapabilityItem key={capability.title} {...capability} />
         ))}
