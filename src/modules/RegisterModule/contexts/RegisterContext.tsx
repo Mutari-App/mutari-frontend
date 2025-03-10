25a51c59b370114a2915710b398aeed4d3fa63fb
matthewhjt <matthewhjt72@gmail.com>
2025-03-10 18:19:14 +0700

    feat: implement RegisterContext for Register Forms

---
+++ src/modules/RegisterModule/contexts/RegisterContext.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { createContext, useContext, useState } from 'react'
+import {
+  FormContextInterface,
+  RegisterContextProviderProps,
+  RegisterDataInterface,
+} from '../interface'
+
+const RegisterContext = createContext({} as FormContextInterface)
+
+export const useRegisterContext = () => useContext(RegisterContext)
+
+export const RegisterContextProvider = ({
+  children,
+}: RegisterContextProviderProps) => {
+  const [page, setPage] = useState<number>(0)
+  const [registerData, setRegisterData] = useState<RegisterDataInterface>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    birthDate: undefined,
+    uniqueCode: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const goToNextPage = () => {
+    setPage((prevState) => ++prevState)
+  }
+
+  const contextValue: FormContextInterface = {
+    page,
+    setPage,
+    goToNextPage,
+    registerData,
+    setRegisterData,
+  }
+
+  return (
+    <RegisterContext.Provider value={contextValue}>
+      {children}
+    </RegisterContext.Provider>
+  )
+}
