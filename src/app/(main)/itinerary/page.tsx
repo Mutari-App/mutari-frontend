943d0d5cecd47212d5bc9134ab297664d0f007b5
mma <111668501+mmalkindi@users.noreply.github.com>
2025-03-13 00:49:30 +0700

    feat: add page names metadata

--- src/app/(main)/itinerary/page.tsx
+++ src/app/(main)/itinerary/page.tsx
@@ -1,4 +1,11 @@
 import ItineraryModule from '@/modules/ItineraryModule'
+import type { Metadata } from 'next'
+
+export function generateMetadata(): Metadata {
+  return {
+    title: 'Itinerary',
+  }
+}
 
 export default function Home() {
   return <ItineraryModule />
