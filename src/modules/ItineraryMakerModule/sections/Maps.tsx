651adf1e84c0987327a539ea63ac38ef8e92ea0b
Fathan <fathan.naufal@ui.ac.id>
2025-03-24 20:34:50 +0700

    feat: add Maps section for itinerary maker

---
+++ src/modules/ItineraryMakerModule/sections/Maps.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
+import type { CreateItineraryDto } from '../interface'
+
+type MapsProps = {
+  readonly itineraryData: Readonly<CreateItineraryDto>
+}
+
+function Maps({ itineraryData }: MapsProps) {
+  const [map, setMap] = React.useState<google.maps.Map | null>(null)
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ''
+  const defaultCenter = React.useMemo(
+    () => ({ lat: -6.3604, lng: 106.82719 }),
+    []
+  )
+  const locations = itineraryData.sections.flatMap(
+    (section) =>
+      section.blocks?.flatMap((block) => {
+        if (!block.location) return []
+        const [lat, lng] = block.location
+          .split(',')
+          .map((coord) => parseFloat(coord.trim()))
+        return { lat, lng }
+      }) ?? []
+  )
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: apiKey,
+    libraries: ['places'],
+  })
+
+  return (
+    <div className="w-full h-full">
+      {!isLoaded ? (
+        <div>Loading...</div>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="w-full h-full"
+          center={defaultCenter}
+          zoom={10}
+          onLoad={(map) => setMap(map)}
+          options={{
+            streetViewControl: false,
+          }}
+        >
+          {locations.map((loc, idx) => (
+            <Marker key={idx} position={loc} data-testid="map-marker" />
+          ))}
+        </GoogleMap>
+      )}
+    </div>
+  )
+}
+
+export default Maps
