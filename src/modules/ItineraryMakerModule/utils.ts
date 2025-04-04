82fb4c83400adfed024c7a033c868147c58e2d2f
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-04-05 04:57:48 +0700

    chore: Implement helper function  to retrieve suggestions based on feedback items

---
+++ src/modules/ItineraryMakerModule/utils.ts
@@ -0,0 +1,19 @@
+import { FeedbackItem } from './interface'
+
+export function feedbackForField(
+  feedbackItems: FeedbackItem[] = [],
+  sectionIndex: number,
+  blockIndex: number,
+  blockType: 'LOCATION' | 'NOTE',
+  field: string
+): string | null {
+  const item = feedbackItems.find(
+    (f) =>
+      f.target.sectionIndex === sectionIndex &&
+      f.target.blockIndex === blockIndex &&
+      f.target.blockType === blockType &&
+      f.target.field === field
+  )
+
+  return item?.suggestion ?? null
+}
