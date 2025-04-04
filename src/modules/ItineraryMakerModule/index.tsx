aac72770109f69217bd6f3d2013f9984404065a2
daffarafip24 <daffarafip24@gmail.com>
2025-04-05 02:54:01 +0700

    feat: Implement feature for invitation

--- src/modules/ItineraryMakerModule/index.tsx
+++ src/modules/ItineraryMakerModule/index.tsx
@@ -36,6 +36,7 @@ import NotFound from 'next/error'
 const SAVED_ITINERARY_KEY = 'saved_itinerary_data'
 
 export default function ItineraryMakerModule() {
+  const { user } = useAuthContext()
   const launchingDate = new Date(
     process.env.NEXT_PUBLIC_LAUNCHING_DATE ?? '2025-01-22T00:00:00'
   )
@@ -109,6 +110,12 @@ export default function ItineraryMakerModule() {
         )
 
         if (res.statusCode !== 200) throw new Error(res.message)
+        if (res.data.userId !== user?.id) {
+          toast.error('Anda tidak memiliki akses untuk mengedit itinerary ini')
+          router.push(`/itinerary/${itineraryId}`)
+          return
+        }
+
         setData(res.data)
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
       } catch (error) {
