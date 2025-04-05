193752bfc435a5d5bd0854b07e3edab7f387186a
fahmi-ramadhan <fahmi122004@gmail.com>
2025-04-05 17:46:46 +0700

    refactor: add max-h-screen and overflow-auto to Detail Itinerary page

--- src/modules/DetailItineraryModule/index.tsx
+++ src/modules/DetailItineraryModule/index.tsx
@@ -42,7 +42,7 @@ export default function DetailItineraryModule() {
 
   return (
     <div className="flex max-h-screen">
-      <div className="container max-w-4xl mx-auto p-4 pt-24 min-h-screen">
+      <div className="container max-w-4xl mx-auto p-4 pt-24 min-h-screen max-h-screen overflow-auto">
         <ItineraryHeader data={data} />
         <ItinerarySummary startDate={data.startDate} endDate={data.endDate} />
         <ItineraryList section={data.sections || []} />
