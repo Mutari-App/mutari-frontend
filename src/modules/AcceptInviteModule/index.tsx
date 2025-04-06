c65e2dbab4e6dec933b8e8f71f8d326ac76dd83a
Muhammad Daffa'I Rafi Prasetyo <88131567+daffarafi@users.noreply.github.com>
2025-04-06 12:54:50 +0700

    fix: Fix when user accept invitation unauthorized (#89)

--- src/modules/AcceptInviteModule/index.tsx
+++ src/modules/AcceptInviteModule/index.tsx
@@ -5,37 +5,49 @@ import { AcceptInvitationInteface, AcceptInviteModuleProps } from './interface'
 import { toast } from 'sonner'
 import { useRouter } from 'next/navigation'
 import { Loader2 } from 'lucide-react'
+import { useAuthContext } from '@/contexts/AuthContext'
 import { customFetch } from '@/utils/newCustomFetch'
 
 export const AcceptInviteModule: React.FC<AcceptInviteModuleProps> = ({
   itineraryId,
 }) => {
+  const { isAuthenticated } = useAuthContext()
   const router = useRouter()
 
-  const getInvitaion = async () => {
-    try {
-      const acceptResponse = await customFetch<AcceptInvitationInteface>(
-        `/itineraries/${itineraryId}/accept-invitation`,
-        {
-          method: 'POST',
-        }
-      )
+  const getInvitation = async () => {
+    if (!isAuthenticated) {
+      toast.error('Silahkan login terlebih dahulu!')
+      router.push(`/login?redirect=/itinerary/${itineraryId}/accept-invite`)
+    } else {
+      try {
+        const acceptResponse = await customFetch<AcceptInvitationInteface>(
+          `/itineraries/${itineraryId}/accept-invitation`,
+          {
+            method: 'POST',
+          }
+        )
 
-      if (acceptResponse.statusCode !== 200) {
-        throw new Error(acceptResponse.message)
+        if (acceptResponse.statusCode !== 200) {
+          throw new Error(acceptResponse.message)
+        }
+        router.push(`/itinerary/${itineraryId}`)
+      } catch (err) {
+        toast.error('Undangan tidak valid!', {
+          description: 'Coba gunakan akun lain atau minta undangan baru',
+        })
+        router.push('/')
       }
-      router.push(`/itinerary/${itineraryId}`)
-    } catch (err) {
-      toast.error('Undangan tidak valid!', {
-        description: 'Coba gunakan akun lain atau minta undangan baru',
-      })
-      router.push('/')
     }
   }
 
   useEffect(() => {
-    void getInvitaion()
-  }, [])
+    const timeout = setTimeout(() => {
+      void getInvitation()
+    }, 0)
+
+    return () => clearTimeout(timeout)
+  }, [toast])
+
   return (
     <div className="flex items-center justify-center h-screen">
       <Loader2 className="animate-spin w-6 h-6 mr-2" />
