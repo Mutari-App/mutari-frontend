c59f80a03026056d907604cdd2648a86c8b72a2f
mma <111668501+mmalkindi@users.noreply.github.com>
2025-04-08 11:19:42 +0700

    feat: add failsafe for itinerary reminder submissionData

--- src/modules/ItineraryMakerModule/index.tsx
+++ src/modules/ItineraryMakerModule/index.tsx
@@ -1633,9 +1633,11 @@ export default function ItineraryMakerModule({
 
     // Submit itinerary reminder changes
     try {
-      const submissionData = {
+      const submissionData: ItineraryReminderDto = {
         ...itineraryReminderData,
         itineraryId: newItineraryId,
+        recipient: user?.email,
+        recipientName: user?.firstName,
       }
       await submitItineraryReminder(submissionData)
     } catch (error) {
