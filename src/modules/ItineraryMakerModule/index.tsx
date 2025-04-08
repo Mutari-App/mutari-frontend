ae9c10856be97543506a6fd699fd70b2ee448c00
matthewhjt <matthewhjt72@gmail.com>
2025-04-08 12:19:58 +0700

    refactor: fix itinerary detail and itinerary maker module show contingency map

--- src/modules/ItineraryMakerModule/index.tsx
+++ src/modules/ItineraryMakerModule/index.tsx
@@ -1989,7 +1989,7 @@ export default function ItineraryMakerModule({
       </div>
       <div className="w-full min-h-screen hidden md:block">
         <Maps
-          itineraryData={itineraryData.sections}
+          itineraryData={contingency?.sections ?? itineraryData.sections}
           addLocationToSection={addLocationToSection}
           isEditing
           positionToView={positionToView}
