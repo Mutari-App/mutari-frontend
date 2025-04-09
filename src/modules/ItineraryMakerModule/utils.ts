657864bd1c45abb75595b9b899db0dbd90e442b4
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-04-09 18:17:51 +0700

    fix: update feedbackForField to use blockId instead of blockIndex

--- src/modules/ItineraryMakerModule/utils.ts
+++ src/modules/ItineraryMakerModule/utils.ts
@@ -3,14 +3,14 @@ import { FeedbackItem } from './interface'
 export function feedbackForField(
   feedbackItems: FeedbackItem[] = [],
   sectionIndex: number,
-  blockIndex: number,
+  blockId: string,
   blockType: 'LOCATION' | 'NOTE',
   field: string
 ): string | null {
   const item = feedbackItems.find((f) => {
     return (
       f.target.sectionIndex === sectionIndex &&
-      f.target.blockIndex === blockIndex &&
+      f.target.blockId === blockId &&
       f.target.blockType === blockType &&
       f.target.field === field
     )
