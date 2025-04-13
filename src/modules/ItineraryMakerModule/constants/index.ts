0736e21bf85a3897e5d37a96bcfbd4f43c588a42
Fathan <fathan.naufal@ui.ac.id>
2025-04-13 12:43:34 +0700

    feat: implement visgl map api library

---
+++ src/modules/ItineraryMakerModule/constants/index.ts
@@ -0,0 +1,12 @@
+export const SECTION_COLORS = [
+  '#b91c1c', // bg-red-700
+  '#15803d', // bg-green-700
+  '#1d4ed8', // bg-blue-700
+  '#a16207', // bg-yellow-700
+  '#7e22ce', // bg-purple-700
+  '#be185d', // bg-pink-700
+  '#c2410c', // bg-orange-700
+  '#0f766e', // bg-teal-700
+  '#4338ca', // bg-indigo-700
+  '#047857', // bg-emerald-700
+]
