9663130d59de4395e145f122c3d148bc84c128b0
Fathan <fathan.naufal@ui.ac.id>
2025-05-10 10:06:06 +0700

    feat: add recently viewed section in Tour page

---
+++ src/modules/TourMarketplaceModule/interface.ts
@@ -0,0 +1,34 @@
+import { type CustomFetchBaseResponse } from '@/utils/newCustomFetch/interface'
+
+export interface RecentlyViewedTourResponse extends CustomFetchBaseResponse {
+  tours: TourView[]
+}
+
+export interface TourView {
+  updatedAt: string
+  createdAt: string
+  id: string
+  userId: string
+  tourId: string
+  viewedAt: string
+  tour: Tour
+}
+
+export interface Tour {
+  id: string
+  title: string
+  coverImage?: string
+  maxCapacity: number
+  description?: string
+  location: string
+  pricePerTicket: string
+  duration: number
+  durationType: 'HOUR' | 'DAY' | 'WEEK'
+  itineraryId: string
+  createdAt: string
+  updatedAt: string
+}
+
+export interface TourCardProps {
+  tour: Tour
+}
