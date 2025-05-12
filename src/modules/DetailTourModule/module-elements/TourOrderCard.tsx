d12e03266ebee2bef4fba4aa96027aa531509a83
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-05-12 22:00:53 +0700

    feat: enhance TourOrderCard to format price per ticket

--- src/modules/DetailTourModule/module-elements/TourOrderCard.tsx
+++ src/modules/DetailTourModule/module-elements/TourOrderCard.tsx
@@ -8,7 +8,7 @@ export const TourOrderCard = ({
   return (
     <div className="sticky top-4 w-full lg:w-72 bg-white p-4 rounded-xl shadow-md self-start">
       <p className="text-xl font-semibold text-gray-800 text-raleway">
-        Rp{pricePerTicket}
+        Rp{pricePerTicket.toLocaleString('id-ID')}
         <span className="text-sm"> / pax</span>
       </p>
       <button className="mt-4 w-full bg-gradient-to-r from-[#016CD7] to-[#014285] text-white py-2 rounded-lg">
