4d7757f05f6ba7bc47b9c2ed83b31863b8fcfb5d
Fathan <fathan.naufal@ui.ac.id>
2025-05-16 16:13:14 +0700

    feat: add recently viewed tour section

--- src/modules/TourMarketplaceModule/index.tsx
+++ src/modules/TourMarketplaceModule/index.tsx
@@ -1,9 +1,11 @@
 import HeaderSection from './sections/HeaderSection'
+import RecentlyViewedTour from './sections/RecentlyViewedTour'
 
 export default function TourMarketplaceModule() {
   return (
     <div className="flex flex-col items-center gap-7 w-full mb-5">
       <HeaderSection />
+      <RecentlyViewedTour />
     </div>
   )
 }
